5ea24113b8814576216201fc35a44ea99fc8af96
neakovalainen <neakovalaine@gmail.com>
2025-03-17 15:54:00 +0200

    Exercises 2.13 and 2.14

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -9,31 +11,12 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 //has to be body, because it's returned by app.post and it's a predefined in express!!
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
+let persons = []
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+      response.json(persons)  
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -98,19 +81,19 @@ app.post('/api/persons', (request, response) => {
             error: 'the person is already there u dum dum'
         })
     }
-    const person = {
+    const person = new Person({
         id: `${newId()}`,
         name: body.name,
         number: body.number || false,
-    }
+    })
 
-    persons = persons.concat(person)
-    console.log(request.body, body)
-    response.json(request.body)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
